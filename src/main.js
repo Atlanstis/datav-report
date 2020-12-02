815fe15131d193dce352e03347e71e858aff1a92
Gypsophila。 <futur_wxc@163.com>
2020-12-02 16:45:59 +0800

    feat: 增加 vue-echarts，v-charts

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import ECharts from 'echarts'
+import VueEcharts from 'vue-echarts'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import './plugins/element.js'
 import './style/index.scss'
 
+Vue.component('v-echarts', VueEcharts)
 Vue.config.productionTip = false
 Vue.prototype.$echarts = ECharts
 
